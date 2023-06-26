523ca117408478b307c32d6ed279044c240f60a6
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2023-06-26 22:45:42 +0100

    i did found a way to add the video but amma try to get it from a link now

--- app/Herobanner.tsx
+++ app/Herobanner.tsx
@@ -1,7 +1,11 @@
 export const Herobanner = () => {
   return (
-  <div className="h-screen p-8 text-white text-4xl md:text-7xl lg:text-8xl font-bold tracking-wider leading-normal sm:leading-relaxed md:leading-[6rem] lg:leading-[7.5rem]">
-    <div>Uniting People in <div className="flex items-center justify-center">S <div className="flex items-center justify-center bg-hidden border-4 border-white w-16 h-7 md:w-32 md:h-16 rounded-full"></div>lidarity</div> and Humanity for a Brighter Future. </div>
+  <div className="my-10 p-8 text-white text-4xl md:text-7xl lg:text-8xl font-bold tracking-wider leading-normal sm:leading-relaxed md:leading-[6rem] lg:leading-[7.5rem]">
+    <div>Uniting People in <div className="flex items-center justify-center tracking-widest ">S 
+        <div className="relative overflow-hidden flex items-center justify-center bg-hidden border-4 border-white w-36 sm:h-16 h-10 md:w-32 md:h-16 rounded-full">
+          <video src='https://drive.google.com/file/d/1YhwYI3Le8FLblUgPxDG4ZNXE6J8buOhM/view?usp=sharing' autoPlay loop muted  />
+        </div>
+        lidarity</div> and Humanity for a Brighter <div className="relative font-serif italic"><div className="absolute left-10 right-10 md:left-20 md:left-20 md:right-20 md:right-10 bottom-0 top-0 border border-white rounded-[50%] -rotate-12" />Future</div> </div>
   </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ return (
     <main className="w-full text-center">
       <Navbar />
       <Herobanner />
-      <div className="absolute bg-[#161616] w-full h-full" />
-      <div className="absolute gradient w-80 h-80 rounded-full -top-36 -left-36 blur-2xl z-1" />
-      <div className="absolute bg-[#161616]/30 w-full h-full backdrop-blur-sm" />
+      <div className="absolute bg-[#161616] w-full" />
+      <div className="absolute gradient w-80 h-80 rounded-full -top-36 -left-36 blur-2xl" />
+      <div className="absolute bg-[#161616]/30 w-full backdrop-blur-sm" />
     </main>
   )
 }
