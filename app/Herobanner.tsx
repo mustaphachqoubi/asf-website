aad5d08acd3bfc72354ee8086eb3038166ef6619
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2023-06-27 23:33:13 +0100

    i did not find the right place to upload autoplayed video yet

--- app/Herobanner.tsx
+++ app/Herobanner.tsx
@@ -3,9 +3,17 @@ export const Herobanner = () => {
   <div className="my-10 p-8 text-white text-4xl md:text-7xl lg:text-8xl font-bold tracking-wider leading-normal sm:leading-relaxed md:leading-[6rem] lg:leading-[7.5rem]">
     <div>Uniting People in <div className="flex items-center justify-center tracking-widest ">S 
         <div className="relative overflow-hidden flex items-center justify-center bg-hidden border-4 border-white w-36 sm:h-16 h-10 md:w-32 md:h-16 rounded-full">
-          <video src='https://drive.google.com/file/d/1YhwYI3Le8FLblUgPxDG4ZNXE6J8buOhM/view?usp=sharing' autoPlay loop muted  />
+       
+
+          <iframe src="https://www.veed.io/embed/ffd8dd08-6d89-42cc-877e-d4b40b84a96d" width="744" height="504" frameborder="0" title="pexels-fauxels-3256543-3840x2160-50fps" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+
         </div>
         lidarity</div> and Humanity for a Brighter <div className="relative font-serif italic"><div className="absolute left-10 right-10 md:left-20 md:left-20 md:right-20 md:right-10 bottom-0 top-0 border border-white rounded-[50%] -rotate-12" />Future</div> </div>
   </div>
   )
 }
+
+
+
+
+
