869f4a9e62b2ed80c1c6a834366edcb9daffa319
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2023-07-12 21:13:18 +0100

    i am gonna rebuild it again

--- app/Whatwedo.tsx
+++ app/Whatwedo.tsx
@@ -1,18 +1,6 @@
 export const Whatwedo = () => {
- 
   return (
     <>
-      <div className="flex-1 flex flex-col-reverse md:flex-row justify-between items-center">
-        <div className="flex-1 h-full flex justify-center items-center "></div>
-        <div className=" flex-1 h-full flex justify-center items-center">two</div>
-      </div>
-
-      <div className="flex-1 flex justify-between items-center z-50">
-        <div className=" flex-1 h-full flex justify-center items-center">two</div>
-        <div className=" flex-1 h-full flex justify-center items-center">one</div>
-      </div>
-      <div className="absolute gradient w-80 h-80 rounded-full bottom-0 right-0 blur-2xl z-10" />
-    </>
+          </>
   )
 }
-
