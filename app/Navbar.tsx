7180ba4d3c523b767f92019047ca0e81b8d2537f
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2023-07-16 21:01:06 +0100

    it's bad but i am gonna fix it later

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,16 +1,25 @@
-import { FiMenu } from 'react-icons'
-import React from 'react'
+import { FiMenu } from "react-icons";
+import React from "react";
 
 const navList = ["Amis sans frontieres", "Home", "What we do", "Contact Us"];
 
 export const Navbar: React.FC = () => {
   return (
     <div className="flex items-center justify-between px-8 text-white">
-
       <div className="grid grid-cols-3 w-full place-content-center z-50 ">
         {navList.map((list, index) => (
-          <a key={index} href={`${index === 1 ? "#home" : index === 2 ? "#wwd" : index === 3 ? "#cu" : "#"}`}
-className={`border-b-2 border-white p-2 text-sm md:text-md 2xl:p-5 2xl:text-2xl 2xl:font-bold flex justify-center text-center items-center ${
+          <a
+            key={index}
+            href={`${
+              index === 1
+                ? "#home"
+                : index === 2
+                ? "#wwd"
+                : index === 3
+                ? "#cu"
+                : "#"
+            }`}
+            className={`border-b-2 border-white p-2 text-sm md:text-md 2xl:p-5 2xl:text-2xl 2xl:font-bold flex justify-center text-center items-center ${
               index === 0
                 ? "col-span-3 font-bold border-t-none p-4 text-md md:text-lg 2xl:text-4xl"
                 : "col-span-3 cursor-pointer col-span-1 md:col-span-1 hover:bg-[#1f2937]/30"
@@ -18,7 +27,7 @@ className={`border-b-2 border-white p-2 text-sm md:text-md 2xl:p-5 2xl:text-2xl 
           >
             {list}
           </a>
-          ))}
+        ))}
       </div>
 
       <div className="absolute gradient w-80 h-80 rounded-full -top-36 -left-36 blur-2xl z-10" />
--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,29 @@
-"use client"
+"use client";
 
 import { Navbar } from "./Navbar";
 import { Herobanner } from "./Herobanner";
-import { Whatwedo } from './Whatwedo'
-import { useEffect, useState } from 'react'
+import { Whatwedo } from "./Whatwedo";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-
   return (
     <main className="w-full text-center">
-
       <div id="#home" className={`sticky top-0 h-screen `}>
         <Navbar />
         <Herobanner />
       </div>
 
-      <div id="wwd" className={`flex flex-col sticky top-0 w-full h-screen p-8 text-white bg-[#161616] `}>
+      <div
+        id="wwd"
+        className={`flex flex-col sticky top-0 w-full h-screen p-8 text-white bg-[#161616] `}
+      >
         <Whatwedo />
       </div>
 
-      <div id="cu" className={`flex sticky top-0 w-full h-screen justify-center items-center text-white bg-[#161616]`}>
+      <div
+        id="cu"
+        className={`flex sticky top-0 w-full h-screen justify-center items-center text-white bg-[#161616]`}
+      >
         contact us
       </div>
     </main>
