6b7baa4d1f6ae87bd7c8eb3fee4890a2d1e3586a
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2023-07-17 21:42:50 +0100

    i am gonna make it minimalist

--- app/Whatwedo.tsx
+++ app/Whatwedo.tsx
@@ -7,43 +7,23 @@ export const Whatwedo = () => {
         <div className="flex-1 h-full flex justify-center items-center w-full">
           one
         </div>
-        <div className=" flex-1 h-full flex justify-center items-center w-full ">
-          <div className="flex grid grid-cols-[153px_30px_30px] w-full h-full md:grid-cols-1 divide-x md:divide-x-0 md:divide-y divide-dashed">
-            <div className="">
-              <div className="md:h-full md:w-5 ">
-                <div className="border border-white rounded-full h-5 w-5 text-sm flex justify-center items-center ">
-                  <AiOutlineArrowLeft />
-                </div>
-              </div>
-            </div>
-            <div className=""> 
-              <div className="md:h-full md:w-5 ">
-                <div className="border border-white rounded-full h-5 w-5 text-sm flex justify-center items-center ">
-                  <AiOutlineArrowRight />
-                </div>
-              </div>
-            </div>
-            <div className=""> 
-              <div className="md:h-full md:w-5 ">
-                <div className="border border-white rounded-full h-5 w-5 text-sm flex justify-center items-center ">
-                  <AiOutlineArrowRight />
-                </div>
-              </div>
-            </div>          
-
-          </div>
-        </div>
       </div>
 
       <div className="flex-1 flex justify-between items-center z-50">
         <div className=" flex-1 h-full flex justify-center items-center w-full">
-          <div className="flex grid grid-cols-1 w-full h-full md:grid-cols-3 md:divide-x divide-y md:divide-y-0 divide-dashed">
-            <div>two</div>
-            <div>two</div>
-            <div>two</div>
+          <div className="text-left flex grid grid-cols-1 w-full h-full md:grid-cols-3 md:divide-x divide-y md:divide-y-0 divide-dashed">
+            <div className="p-2"> 
+              Our actions create meaningful connections between people through solidarity and mutual support.
+            </div>
+            <div className="p-2"> 
+              We facilitate volunteering opportunities to help communities in need, fostering empathy and love.
+            </div>
+            <div className="p-2"> 
+              Together, we build a better world by providing food, shelter, and education to the less fortunate.
+            </div>
           </div>
         </div>
-        <div className=" flex-1 h-full flex justify-center items-center w-full">
+        <div className=" flex-1 h-full hidden md:flex justify-center items-center w-full">
           one
         </div>
       </div>
