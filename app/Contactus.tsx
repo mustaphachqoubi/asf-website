45122f146647ad6b3e9a4dffd62bfbeaa6f2f9db
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2023-07-23 21:50:09 +0100

    the contact us section is good now, i still have to build a little game in it

--- app/Contactus.tsx
+++ app/Contactus.tsx
@@ -22,13 +22,13 @@ export const Contactus = () => {
   const cards = [
     {
       id: 1,
-      img: "https://i.ytimg.com/vi/82zWdrmnKME/maxresdefault.jpg",
-      sentence: "Watch"
+      img: "https://marketplace.canva.com/EAFFt_DccoA/1/0/1600w/canva-colorful-freelancer-youtube-thumbnail-ucA52BZ7Bqk.jpg",
+      sentence: "Visit"
     },
     {
       id: 1,
-      img: "https://marketplace.canva.com/EAFFt_DccoA/1/0/1600w/canva-colorful-freelancer-youtube-thumbnail-ucA52BZ7Bqk.jpg",
-      sentence: "Visit"
+      img: "https://i.ytimg.com/vi/82zWdrmnKME/maxresdefault.jpg",
+      sentence: "Watch"
     },
 
     {
@@ -41,10 +41,14 @@ export const Contactus = () => {
   return (
   <>
     <div className="w-full h-full">
-      <div className='w-full h-full grid grid-cols-1 divide-y-2 md:divide-y-0 md:grid-cols-2 md:divide-x-2'>
-        <div className="flex-1 p-6 flex justify-center items-center">one</div>
-        <div className="flex-1 p-6 flex justify-between flex-col md:flex-row "> 
-          <div className="w-full md:w-10 md:h-full flex flex-row md:flex-col gap-5 justify-center">
+      <div className='w-full h-full flex flex-col md:grid grid-cols-1 divide-y-2 md:divide-y-0 md:grid-cols-2 md:divide-x-2'>
+        <div className="flex-1 p-6 flex flex-col justify-between md:justify-center md:gap-60 items-center">
+            <div className="text-sm md:text-md lg:text-lg xl:text-2xl"> Play to win email and phone number </div>
+            <div>game</div>
+        </div>
+
+         <div className="flex-1  p-6 flex flex-col md:flex-row">
+              <div className="w-full md:w-10 md:h-full flex flex-row md:flex-col gap-5 justify-center ">
              {icons.map(icon => (
              <a href={icon.link} target="_blank">
               <div key={icon.id} className="border-2 rounded-lg text-lg p-2 flex justify-center items-center w-10 h-10 hover:bg-white hover:text-[#121212] hover:border-[#121212] cursor-pointer duration-300">{icon.icon}</div>
@@ -52,7 +56,20 @@ export const Contactus = () => {
             ))} 
           </div>
 
-            <div className="w-full h-full flex md:flex-col justify-between items-center ">
+            <div className="relative md:flex flex-col justify-center gap-10 hidden ">
+              <img src={"/arrowOne.svg"} /> 
+              <img src={"/arrowThree.svg"} /> 
+              <img src={"/arrowTwo.svg"} /> 
+            </div>
+
+            <div className="relative flex justify-center gap-10 md:hidden">
+              <img src={"/arrowtwosmall.svg"} /> 
+              <img src={"/link.svg"} className="absolute left-0 top-2"/> 
+              <img src={"/by.svg"} className="absolute left-0 bottom-2"/> 
+              <img src={"/yourself.svg"} className="absolute right-0"/> 
+            </div>
+
+            <div className=" md:h-full flex md:flex-col md:justify-center justify-between md:gap-10 items-center ">
              {
                 cards.map(card => (
                 <div key={card.id} className="border-2 flex flex-col gap-2 rounded-lg w-52 p-1 md:p-2">
@@ -65,7 +82,7 @@ export const Contactus = () => {
         </div>
       </div>
 
-      <div className="text-xs border-t-2 p-2 flex justify-center bg-[#121212]">
+      <div className="text-xs border-t-2 p-2 flex justify-center bg-[#121212] absolute left-0 right-0">
           <p className="flex gap-2 items-center">built with <span className="text-lg"><AiFillHeart /></span> By <a target="_blank" href="https://mustaphachqoubi.me" className='font-bold underline hover:no-underline'>@Mustaphachqoubi</a> </p>
       </div>
     </div>
